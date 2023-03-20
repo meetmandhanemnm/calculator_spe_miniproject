88ad46b03f011b8998ad66a64eb4e7e77bde8f6f
Meet Mandhane <meetmandhanemnm@gmail.com>
2023-03-20 23:37:51 +0530

    Added square_root and factorial

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -7,14 +7,18 @@ import org.apache.logging.log4j.Logger;
 public class Calculator {
     private static final Logger logger = LogManager.getLogger(Calculator.class);
 
-    public static int add(int a, int b) {
-        logger.info("In add function!!");
-        return a+b;
+    public static double square_root(int a) {
+        logger.info("In square_root function!!");
+        return Math.sqrt(a);
     }
 
-    public static int sub(int a, int b) {
-        logger.info("In subq function!!");
-        return a-b;
+    public static int factorial(int a) {
+        logger.info("In factorial function!!");
+        int res = a;
+        for(int i=a-1;i>=1;i--) {
+            res *= i;
+        }
+        return res;
     }
     public static void main(String[] args) {
 
@@ -23,23 +27,21 @@ public class Calculator {
 
         while(opt!=3) {
             logger.info("Taking input from user");
-            System.out.println("Enter a choice: \n1. Addition\n2. Subtraction\n3. Exit");
+            System.out.println("Enter a choice: \n1. Square Root\n2. Factorial\n3. Exit");
             opt = sc.nextInt();
             int a,b;
             switch (opt) {
                 case 1:
-                    System.out.println("Enter two numbers:");
+                    System.out.println("Enter a number to perform square root:");
                     a = sc.nextInt();
-                    b = sc.nextInt();
                     logger.info("CASE 1");
-                    System.out.println("The result is: " + add(a, b));
+                    System.out.println("Square root of "+a+" is: " + square_root(a));
                     break;
                 case 2:
-                    System.out.println("Enter two numbers:");
+                    System.out.println("Enter a number to perform factorial:");
                     a = sc.nextInt();
-                    b = sc.nextInt();
                     logger.info("CASE 2");
-                    System.out.println("The result is: " + sub(a, b));
+                    System.out.println("The factorial of" +a+" is: " + factorial(a));
                     break;
             }
         }
