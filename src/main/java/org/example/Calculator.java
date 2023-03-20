4bab632311fe8d5762aec4e34f1e63adeb2ee750
Meet Mandhane <meetmandhanemnm@gmail.com>
2023-03-20 23:14:32 +0530

    Added logging support

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,14 +1,19 @@
 package org.example;
 
 import java.util.Scanner;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Calculator {
+    private static final Logger logger = LogManager.getLogger(Calculator.class);
 
     public static int add(int a, int b) {
+        logger.info("In add function!!");
         return a+b;
     }
 
     public static int sub(int a, int b) {
+        logger.info("In subq function!!");
         return a-b;
     }
     public static void main(String[] args) {
@@ -17,21 +22,26 @@ public class Calculator {
         int opt=1;
 
         while(opt!=3) {
-            System.out.println("Enter two numbers:");
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+            logger.info("Taking input from user");
             System.out.println("Enter a choice: \n1. Addition\n2. Subtraction\n3. Exit");
             opt = sc.nextInt();
+            int a,b;
             switch (opt) {
                 case 1:
+                    System.out.println("Enter two numbers:");
+                    a = sc.nextInt();
+                    b = sc.nextInt();
+                    logger.info("CASE 1");
                     System.out.println("The result is: " + add(a, b));
                     break;
                 case 2:
+                    System.out.println("Enter two numbers:");
+                    a = sc.nextInt();
+                    b = sc.nextInt();
+                    logger.info("CASE 2");
                     System.out.println("The result is: " + sub(a, b));
                     break;
             }
         }
-
     }
-
 }
