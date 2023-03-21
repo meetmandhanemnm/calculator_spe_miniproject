e3c51be814b130fc14add8e5079716b6ddfabeae
Meet Mandhane <meetmandhanemnm@gmail.com>
2023-03-21 10:35:18 +0530

    Added log and pow functions

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -20,6 +20,17 @@ public class Calculator {
         }
         return res;
     }
+
+    public static double natural_log(double a) {
+        logger.info("In natural log function!!");
+        return Math.log(a);
+    }
+
+    public static double power(double a, double b) {
+        logger.info("In power function!!");
+        return Math.pow(a,b);
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -27,9 +38,10 @@ public class Calculator {
 
         while(opt!=3) {
             logger.info("Taking input from user");
-            System.out.println("Enter a choice: \n1. Square Root\n2. Factorial\n3. Exit");
+            System.out.println("Enter a choice: \n1. Square Root\n2. Factorial\n3. Natural log\n4. Power\n5. Exit");
             opt = sc.nextInt();
             int a,b;
+            double x,y;
             switch (opt) {
                 case 1:
                     System.out.println("Enter a number to perform square root:");
@@ -42,6 +54,19 @@ public class Calculator {
                     a = sc.nextInt();
                     logger.info("CASE 2");
                     System.out.println("The factorial of " +a+" is: " + factorial(a));
+                    break;
+                case 3:
+                    System.out.println("Enter a number to perform natural log:");
+                    x = sc.nextDouble();
+                    logger.info("CASE 3");
+                    System.out.println("The natural log of " +x+" is: " + natural_log(x));
+                    break;
+                case 4:
+                    System.out.println("Enter a,b to perform power operation (a^b):");
+                    x = sc.nextDouble();
+                    y = sc.nextDouble();
+                    logger.info("CASE 4");
+                    System.out.println("Power operation "+x+" ^ "+y+" results in : " + power(x,y));
                     break;
             }
         }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -4,9 +4,7 @@ import org.example.Calculator;
 public class CalculatorTest {
 
     @Test
-    public void TestFact1() {
-        Assert.assertEquals(2,Calculator.factorial(2));
-    }
+    public void TestFact1() { Assert.assertEquals(2,Calculator.factorial(2));}
 
     @Test
     public void TestFact2() {
@@ -22,4 +20,17 @@ public class CalculatorTest {
     public void TestRoot2() {
         Assert.assertEquals(9.0,Calculator.square_root(81),0.0f);
     }
+
+    @Test
+    public void Testlog1() { Assert.assertEquals(1.0,Calculator.natural_log(2.718),0.01f);}
+
+    @Test
+    public void Testlog2() { Assert.assertEquals(2.302,Calculator.natural_log(10),0.01f);}
+
+    @Test
+    public void Testpow1() { Assert.assertEquals(4.0,Calculator.power(2.0,2.0),0.01f);}
+
+    @Test
+    public void Testpow2() { Assert.assertEquals(1.0,Calculator.power(2.0,0.0),0.01f);}
+
 }
